90ac35ea811b69bbeb1a10f782fa4a9237219cce
Jonathan M. <neodev7440@gmail.com>
2026-03-25 15:43:06 +0100

    ajout du dao pour booking + les méthodes nécessaires pour les besoin du Tp

---
+++ src/main/java/fr/fms/RoomServiceJpa/dao/BookingRepository.java
@@ -0,0 +1,20 @@
+package fr.fms.RoomServiceJpa.dao;
+
+import fr.fms.RoomServiceJpa.entities.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByRoomName(String name);
+    List<Booking> findByDate(LocalDate date);
+    List<Booking> findByRoomCapacity(int capacity);
+    List<Booking> findByRoomIdAndDateAndStartTimeLessThanAndEndTimeGreaterThan(
+            Long roomId,
+            LocalDate date,
+            LocalTime endTime,
+            LocalTime startTime
+    );
+}
