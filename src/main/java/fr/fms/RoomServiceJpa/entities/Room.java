e16ad07df2128e6ad8d72a222f12f8ac07c2e126
Jonathan M. <neodev7440@gmail.com>
2026-03-25 15:04:46 +0100

    ajout de l'entité room qui représente une salle

---
+++ src/main/java/fr/fms/RoomServiceJpa/entities/Room.java
@@ -0,0 +1,50 @@
+package fr.fms.RoomServiceJpa.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Room  implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int capacity;
+
+    //----------------------------constructors----------------------
+    public Room() {}
+
+    public Room(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    //-------------------getter and setter-----------------------------------
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
