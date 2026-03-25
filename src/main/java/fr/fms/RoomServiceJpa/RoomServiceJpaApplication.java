0dc84d34d5a8ce7efd5d2ad7f3103bc4657113c9
Jonathan M. <neodev7440@gmail.com>
2026-03-25 14:59:04 +0100

    first commit

---
+++ src/main/java/fr/fms/RoomServiceJpa/RoomServiceJpaApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.RoomServiceJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServiceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomServiceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fms/RoomServiceJpa/RoomServiceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.RoomServiceJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServiceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
